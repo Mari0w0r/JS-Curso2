b4bbd47cd80a71b6fc043a6bb4bfb657ee77f4a9
Mari0w0r <marianaorregony@gmail.com>
2023-01-27 15:40:57 -0500

    Se agregó Fetch y librerías

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,17 @@
+//DEFINICION DE VARIABLES Y CARGA DE DATOS
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+//DATOS DE JSON Y FETCH 
+const url = "http://127.0.0.1:5501/js/data.json";
+
+fetch(url)
+.then(res => res.json())
+.then(data => {renderizarProductos(data)
+    prods = data;
+})
+
+//VARIABLES 
+let prods = 0;
 let iconoCarrito = document.querySelector('#carrito-icono')
 let carritoClase = document.querySelector('.carrito');
 let cerrarCarrito = document.querySelector('#cerrar-carrito')
@@ -5,52 +19,39 @@ const itemsCarritoEl = document.querySelector(".carrito-contenido");
 const productsEl = document.querySelector(".productos");
 const subtotalEl = document.querySelector(".total");
 
+//ARRAY DEL CARRITO
 
-//ABRIR CARRITO 
+let carrito = JSON.parse(localStorage.getItem("CARRITO")) || [];
+actualizarCarrito();
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+//ABRIR Y CERRAR CARRITO
 iconoCarrito.onclick = () => {
     carritoClase.classList.add("active");
 }
-
-//CERRAR CARRITO 
 
 cerrarCarrito.onclick = () => {
     carritoClase.classList.remove("active");
 }
 
-function renderizarProductos(){
-    productos.forEach((producto) => {
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+//FUNCIONES DE FUNCIONAMIENTO DEL CARRITO Y HTML
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
+// RENDERIZAR PRODUCTOS
+function renderizarProductos(productos){
+    productos.forEach((prod) => {
         productsEl.innerHTML += `
         <div class="producto-caja">
-            <img src="${producto.imgsrc}" alt="" class="producto-imagen">
-            <h2 class="producto-titulo">${producto.name}</h2>
-            <span class="precio">${producto.precio}</span>
-            <i class='bx bx-shopping-bag añadir-carrito' onclick="añadirAlCarrito(${producto.id})"></i>
+            <img src="${prod.imgsrc}" alt="" class="producto-imagen">
+            <h2 class="producto-titulo">${prod.name}</h2>
+            <span class="precio">COP${prod.precio}</span>
+            <i class='bx bx-shopping-bag añadir-carrito' onclick="añadirAlCarrito(${prod.id})"></i>
         </div>
         `
     })
-}
-renderizarProductos();
-
-//cart array
-
-let carrito = JSON.parse(localStorage.getItem("CARRITO")) || [];
-actualizarCarrito();
-
-//AÑADIR AL CARRITO
-function añadirAlCarrito(id){
-    //CHEQUEAR SI EL PRODUCTO YA EXISTE
-    if(carrito.some((elemento) => elemento.id === id)){
-        cambiarNumeroDeUnidades("mas", id)
-    } else {
-        const elemento = productos.find((producto) => producto.id === id)
-
-        carrito.push({
-            ...elemento,
-            numeroDeUnidades: 1,
-        })
-    }
-
-    actualizarCarrito();
 }
 
 //ACTUALIZAR CARRITO
@@ -62,46 +63,60 @@ function actualizarCarrito(){
     localStorage.setItem("CARRITO", JSON.stringify(carrito));
 }
 
-//CALCULAR TOTAL
-function renderizarSubtotal(){
-    let precioTotal = 0, totalElementos = 0;
+//BOTON DE COMPRA
+function btnCompraClicked(){
 
-    carrito.forEach((elemento) =>{
-        precioTotal += elemento.precio * elemento.numeroDeUnidades;
-        totalElementos += elemento.numeroDeUnidades;
+    if (itemsCarritoEl.hasChildNodes()){
 
-        subtotalEl.innerHTML = `
-        <div class="total-titulo">TOTAL</div>
-        <div class="total-precio">COP${precioTotal}(${totalElementos} productos)</div>
-        `
-    })
+        itemsCarritoEl.removeChild(itemsCarritoEl.firstChild);
+
+        localStorage.removeItem("CARRITO");
+
+        carrito = []
+
+        itemsCarritoEl.innerHTML = "";
+
+        Swal.fire(
+            'Compra exitosa',
+            'El pedido llegará en 2 días',
+            'success'
+        )
+        
+    } else {
+        Swal.fire(
+            'Carrito vacío',
+            'Por favor agrega un producto al carrito',
+            'error'
+        )
+    }
+
+    subtotalEl.innerHTML = `
+    <div class="total-titulo">TOTAL</div>
+    <div class="total-precio">COP0</div>
+    `
 }
 
+//AÑADIR, ELIMINAR Y CAMBIAR CANTIDADES DENTRO DEL CARRITO
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
-//RENDERIZAR ELEMENTOS DEL CARRITO
-function renderizarElementosDelCarrito(){
-    console.log(carrito);
-    itemsCarritoEl.innerHTML = "";
-    carrito.forEach((elemento) => {
-        itemsCarritoEl.innerHTML +=`
-        <div class="carrito-box">
-            <img src="${elemento.imgsrc}" alt="" class="carrito-imagen">
-                <div class="detalles-container">
-                    <div class="carrito-producto-titulo">${elemento.name}</div>
-                    <div class="producto-precio">COP${elemento.precio}</div>
-                        <div class="unidades">
-                            <i class='bx bx-minus cambiar-cantidad' onclick="cambiarNumeroDeUnidades('menos', ${elemento.id})"></i>
-                            <div class="number">${elemento.numeroDeUnidades}</div>
-                            <i class='bx bx-plus cambiar-cantidad' onclick="cambiarNumeroDeUnidades('mas', ${elemento.id})"></i>          
-                        </div>
-                </div>
-                    <i class='bx bxs-trash-alt carrito-eliminar' onclick="eliminarElementosDelCarrito(${elemento.id})"></i>
-        </div>
-        `
-    });
+//AÑADIR
+function añadirAlCarrito(id){
+    //CHECAR SI EL PRODUCTO YA EXISTE
+    if(carrito.some((elemento) => elemento.id === id)){
+        cambiarNumeroDeUnidades("mas", id)
+    } else {
+        const elemento = prods.find((producto) => producto.id === id)
+
+        carrito.push({
+            ...elemento,
+            numeroDeUnidades: 1,
+        })
+    }
+
+    actualizarCarrito();
 }
 
-//ELIMINAR OBJETOS DEL CARRITO
+//ELIMINAR
 function eliminarElementosDelCarrito(id){
     carrito = carrito.filter((elemento) => elemento.id !== id)
 
@@ -137,18 +152,49 @@ function cambiarNumeroDeUnidades(accion, id){
 
     actualizarCarrito();  
 }
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
-function btnCompraClicked(){
-    while (itemsCarritoEl.hasChildNodes()){
-        itemsCarritoEl.removeChild(itemsCarritoEl.firstChild)
-    }
-    alert("Tu Compra ha sido realizada")
+//FUNCIONES DE REDENRIZADO DEL CARRITO
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 
-    subtotalEl.innerHTML = `
-    <div class="total-titulo">TOTAL</div>
-    <div class="total-precio">COP0</div>
-    `
-    localStorage.removeItem("CARRITO");
-    }
+//SUBTOTAL
+function renderizarSubtotal(){
+    let precioTotal = 0, totalElementos = 0;
+
+    carrito.forEach((elemento) =>{
+        precioTotal += elemento.precio * elemento.numeroDeUnidades;
+        totalElementos += elemento.numeroDeUnidades;
+
+        subtotalEl.innerHTML = `
+        <div class="total-titulo">TOTAL</div>
+        <div class="total-precio">COP${precioTotal}(${totalElementos} productos)</div>
+        `
+    })
+}
+
+//ELEMENTOS DEL CARRITO
+function renderizarElementosDelCarrito(){
+    console.log(carrito);
+    itemsCarritoEl.innerHTML = "";
+    carrito.forEach((elemento) => {
+        itemsCarritoEl.innerHTML +=`
+        <div class="carrito-box">
+            <img src="${elemento.imgsrc}" alt="" class="carrito-imagen">
+                <div class="detalles-container">
+                    <div class="carrito-producto-titulo">${elemento.name}</div>
+                    <div class="producto-precio">COP${elemento.precio}</div>
+                        <div class="unidades">
+                            <i class='bx bx-minus cambiar-cantidad' onclick="cambiarNumeroDeUnidades('menos', ${elemento.id})"></i>
+                            <div class="number">${elemento.numeroDeUnidades}</div>
+                            <i class='bx bx-plus cambiar-cantidad' onclick="cambiarNumeroDeUnidades('mas', ${elemento.id})"></i>          
+                        </div>
+                </div>
+                    <i class='bx bxs-trash-alt carrito-eliminar' onclick="eliminarElementosDelCarrito(${elemento.id})"></i>
+        </div>
+        `
+    });
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
